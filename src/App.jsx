bcfb1607d450d9ebbbde422a1b1c06fcbfe6fd7c
Cesar Revolledo <wrevolledo@cr-ingenieros.com>
2026-02-25 12:19:16 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ import MisPagos from "./pages/MisPagos.jsx";
 import PagoMulta from "./pages/PagoMulta.jsx";
 import MisMultas from "./pages/MisMultas.jsx";
 import EstadosCuenta from "./pages/EstadosCuenta.jsx";
-import Fotografias from "./pages/fotografias.jsx";
+import fotografias from "./pages/fotografias.jsx";
 
 export default function App() {
   return (
