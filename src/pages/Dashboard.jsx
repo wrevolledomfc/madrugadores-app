4e2ea46ce9ea3ecf677b5e33cc846cbd5f74cb24
Cesar Revolledo <wrevolledo@cr-ingenieros.com>
2026-03-01 18:55:02 -0500

    update

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -9,6 +9,7 @@ import { cn } from "../lib/cn";
 import madrugadoresLogo from "../assets/madrugadores-logo.png";
 import superligaBanner from "../assets/superliga-banner-equipos.png";
 import SponsorCarousel from "../components/SponsorCarousel";
+import jugadores from "../assets/jugadores.png";
 
 /* ✅ ICONOS OFICIALES DE MARCA */
 import { FaWhatsapp, FaFacebookF, FaTiktok } from "react-icons/fa";
